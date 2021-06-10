16213b28edfdb94cd2a19622405de4299c183a38
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:28:31 +0530

    Add files via upload

---
+++ LINQ All/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_All1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using Method Syntax
+            bool MSResult = Student.GetAllStudnets().All(std => std.TotalMarks > 250);
+            //Using Query Syntax
+            bool QSResult = (from std in Student.GetAllStudnets()
+                             select std).All(std => std.TotalMarks > 250);
+            Console.WriteLine(MSResult);
+            Console.ReadKey();
+        }
+    }
+}
