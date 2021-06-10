b08cc25fd159a1149946b2794fcf3ae0401df3a7
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:32:26 +0530

    Add files via upload

---
+++ LINQ Contains/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] IntArray = { 11, 22, 33, 44, 55 };
+            //Using Method Syntax
+            var IsExistsMS = IntArray.Contains(33);
+            //Using Query Syntax
+            var IsExistsQS = (from num in IntArray
+                              select num).Contains(33);
+            Console.WriteLine(IsExistsMS);
+            Console.ReadKey();
+        }
+    }
+}
