4fb827af16307a3c0dd6461eb12d19eaae39d244
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:30:21 +0530

    Add files via upload

---
+++ LINQ Any/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Any1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "James", "Sachin", "Sourav", "Pam", "Sara" };
+            //Method Syntax
+            var ResultMS = stringArray.Any(name => name.Length > 5);
+            //Query Syntax
+            var ResultQS = (from name in stringArray
+                            select name).Any(name => name.Length > 5);
+            Console.WriteLine("Is Any name with length  greater than 5 Characters : " + ResultMS);
+            Console.ReadKey();
+        }
+    }
+}
